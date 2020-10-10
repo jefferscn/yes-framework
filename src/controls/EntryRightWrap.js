930e7306490bd4fad2fc27a836929388d612973b
jefferscn <16450382@qq.com>
2020-10-10 09:47:36 +0800

    EntryRightWrap bug

--- src/controls/EntryRightWrap.js
+++ src/controls/EntryRightWrap.js
@@ -3,6 +3,10 @@ import { Svr } from 'yes-core';
 
 export default (Comp) => {
     class EntryRightWrap extends PureComponent {
+        state = {
+            allEntry: null,
+            init: false,
+        }
         async componentDidMount() {
             const paras = {
                 cmd: "GetEntry",
@@ -11,7 +15,11 @@ export default (Comp) => {
             const list = await Svr.Request.getSyncData(Svr.SvrMgr.ServletURL, paras);
             let allEntry = new Set();
             this.initAllEntrys(list.entry, allEntry);
-            this.allEntry = allEntry;
+            // this.allEntry = allEntry;
+            this.setState({
+                allEntry: allEntry,
+                init: true,
+            });
         }
         initAllEntrys = (entry, result) => {
             if (!entry) {
@@ -26,10 +34,10 @@ export default (Comp) => {
             }
         }
         hasEntryRight = (entry) => {
-            return (entryKey && this.allEntry) ? this.allEntry.has(entry) : false;
+            return (entry && this.state.allEntry) ? this.state.allEntry.has(entry) : false;
         }
         render() {
-            return <Comp hasEntryRight={this.hasEntryRight} {...this.props} />;
+            return <Comp init={this.state.init} hasEntryRight={this.hasEntryRight} {...this.props} />;
         }
     }
     return EntryRightWrap;
