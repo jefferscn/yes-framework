7923313511143a710d714630d8a1167b2862c99c
jefferscn <16450382@qq.com>
2020-10-14 14:53:31 +0800

    增加ModalTemplate demo

---
+++ src/stories/ModalTemplate.stories.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ModalTemplate from '../template/ModalTemplate';
+import fontAwesome from 'react-native-vector-icons/Fonts/FontAwesome.ttf';
+import { injectFont } from 'yes-web/dist/webutil';
+import 'antd-mobile/dist/antd-mobile.css';
+import StoryWrapper from './StoryWrapper';
+
+injectFont(fontAwesome, 'FontAwesome');
+
+export default {
+  title: 'yes-framework/ModalTemplate',
+  component: ModalTemplate,
+};
+
+const Template = (args) => (
+  <StoryWrapper>
+    <ModalTemplate {...args} />
+  </StoryWrapper>
+)
+const argTypes = {
+  popup: {
+    description: '是否popup模式',
+    defaultValue: true,
+  },
+}
+export const Base = Template.bind({});
+Base.argTypes = argTypes;
+Base.args = {
+    formStatus: 'ok',
+    popup: true,
+    content: {
+        type: "element",
+        elementType: "CellLayoutTemplate",
+        elementProps: {
+            items: ['dict1', 'combbox1']
+        }
+    },
+    actions: [{
+        text: '确定',
+        yigoid: 'button1',
+    },{
+        text: '取消',
+        yigoid: 'button1',
+    }]
+};
