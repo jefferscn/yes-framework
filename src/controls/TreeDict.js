4ce9957b75969fe578873acd8b48f1fb2fbd56c3
jefferscn <16450382@qq.com>
2021-01-19 15:24:35 +0800

    TreeDict控件

--- src/controls/TreeDict.js
+++ src/controls/TreeDict.js
@@ -1,7 +1,8 @@
 import React, { PureComponent } from 'react';
-import { NavBar, Icon, Modal } from 'antd-mobile';
+import { NavBar, Modal } from 'antd-mobile';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import PropTypes from 'prop-types';
-import { View, StyleSheet, TouchableHighlight, ActivityIndicator, Text } from 'react-native';
+import { View, StyleSheet, TouchableHighlight, ScrollView, ActivityIndicator, Text } from 'react-native';
 import { DictWrap } from 'yes-intf';
 import { TriggerControlWrap } from 'yes-comp-react-native-web';
 
@@ -14,25 +15,52 @@ const styles = StyleSheet.create({
     },
     crumbs: {
         height: 40,
+        flexDirection: 'row',
         backgroundColor: 'aliceblue',
-        justifyContent: 'center',
-        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
         paddingLeft: 12,
         paddingRight: 12,
     },
     content: {
         flex: 1
     },
+    dictitem: {
+        flexDirection: 'row',
+        paddingTop: 6,
+        paddingBottom: 6,
+        paddingLeft: 12,
+        paddingRight: 12,
+        alignItems: 'center',
+    },
+    dictitemtext: {
+        textAlign: 'left',
+        flex: 1,
+    },
+    dictitemcheck: {
+        width: 30,
+        color: 'blueviolet',
+    },
+    content: {
+        flex: 1,
+    },
+    crumbtext: {
+        color: 'rgb(33, 150, 243)',
+    },
+    confirm: {
+        fontSize: 24,
+        color: 'white',
+    },
 });
 
 const DictCrumb = ({ item, onPress }) => {
-    onPress = () => {
+    const onPress_ = () => {
         onPress && onPress(item);
     }
     return (
-        <TouchableHighlight onPress={onPress}>
+        <TouchableHighlight onPress={onPress_}>
             <View style={styles.crumb}>
-                <Text>{item.text}</Text>
+                <Text style={styles.crumbtext}>{item.Name}</Text>
                 <Text>/</Text>
             </View>
         </TouchableHighlight>
@@ -46,10 +74,9 @@ class DictCrumbs extends PureComponent {
         this.props.onPress && this.props.onPress();
     }
     render() {
-        const { root, path } = this.props;
+        const { path } = this.props;
         return (
             <View style={styles.crumbs}>
-                <DictCrumb item={{ text: root }} onPress={this.props.onPress} />
                 {
                     path.map((item) => <DictCrumb item={item} onPress={this.props.onPress} />)
                 }
@@ -58,7 +85,7 @@ class DictCrumbs extends PureComponent {
     }
 }
 
-const DictItem = ({ item, onPress }) => {
+const DictItem = ({ item, onPress, checked }) => {
     const { NodeType } = item;
     const onDictItemPress = () => {
         onPress && onPress(item);
@@ -66,7 +93,10 @@ const DictItem = ({ item, onPress }) => {
     return (
         <TouchableHighlight onPress={onDictItemPress}>
             <View style={styles.dictitem}>
-                <Text>{item.text}</Text>
+                {
+                    checked ? <Icon name="check" style={styles.dictitemcheck} /> : null
+                }
+                <Text style={styles.dictitemtext}>{item.Name}</Text>
                 {
                     NodeType === 1 ? <Icon name="chevron-right" /> : null
                 }
@@ -79,11 +109,14 @@ const DictItem = ({ item, onPress }) => {
  */
 class DictItemList extends PureComponent {
     render() {
-        const { items, onDictItemSelect } = this.props;
-        <View style={styles.dictitemlist}>
+        const { items, onDictItemSelect, value } = this.props;
+        if (!items) {
+            return null;
+        }
+        return <View style={styles.dictitemlist}>
             {
                 items.map((item) =>
-                    <DictItem item={item} onPress={onDictItemSelect} />
+                    <DictItem checked={item.oid == value} item={item} onPress={onDictItemSelect} />
                 )
             }
         </View>
@@ -100,8 +133,8 @@ export default class TreeDict extends PureComponent {
         data: null,
         loading: false,
         loadingItem: null,
-        path: [{ oid: "0", itemKey: this.props.itemKey, caption: this.props.caption, NodeType: 1 }],
-        selectedItem: { oid: "0", itemKey: this.props.itemKey, caption: this.props.caption, NodeType: 1 },
+        path: [],
+        value: this.props.value,
     }
     plainData = null
     async loadChildren(item) {
@@ -128,37 +161,85 @@ export default class TreeDict extends PureComponent {
      */
     async componentDidMount() {
         const { value, getParentPath } = this.props;
-        if (value) {
-            //如果有初始值
-            //1.读取当前value对应的字典项目的父路径
-            const result = await getParentPath(value);
-            const path = [];
-            let selectedItem = null;
-            //2.加载路径上的所有数据
-            for (let item of result) {
-                let itm = item;
-                if (itm.oid === "0") {
-                    itm = {
-                        oid: "0",
-                        itemKey: this.props.itemKey,
-                        caption: this.props.caption,
-                        NodeType: 1,
-                    };
+        this.setState({
+            loading: true,
+        });
+        try {
+            if (value) {
+                //如果有初始值
+                //1.读取当前value对应的字典项目的父路径
+                const result = await getParentPath(value);
+                const path = [];
+                // let selectedItem = null;
+                //2.加载路径上的所有数据
+                for (let item of result) {
+                    let itm = item;
+                    if (itm.oid === "0") {
+                        itm = {
+                            oid: "0",
+                            itemKey: this.props.itemKey,
+                            Name: this.props.caption,
+                            NodeType: 1,
+                        };
+                    }
+                    await this.loadChildren(itm);
                 }
-                await this.loadChildren(itm);
+                //3.更新state信息
+                this.setState({
+                    path,
+                });
+            } else {
+                //如果没有初始值，则直接读取根节点数据
+                const item = { oid: "0", itemKey: this.props.itemKey, Name: this.props.caption, NodeType: 1 }
+                await this.loadChildren(item);
+                this.setState({
+                    path: [item],
+                });
+
             }
-            selectedItem = path[path.length - 1];
-            //3.更新state信息
+        } catch (ex) {
+
+        } finally {
             this.setState({
-                path,
-                selectedItem,
+                loading: false,
             });
-        } else {
-            //如果没有初始值，则直接读取根节点数据
         }
     }
-    onSelectCrumb = (item) => {
+    onSelectItem = async (item) => {
+        if (item.NodeType === 1) {//非叶子节点
+            if (this.state.path.includes(item)) {//如果选中项目在当前的路径中
+                const idx = this.state.path.indexOf(item);
+                this.setState({
+                    path: this.state.path.slice(0, idx+1),
+                });
+            } else {//不在当前路径中，则是在当前路径下继续下行一个层次
+                this.setState({
+                    loading: true,
+                });
+                try {
+                    if(!item.children) {
+                        await this.loadChildren(item);
+                    }
+                    this.setState({
+                        path: [...this.state.path, item],
+                    });
+                } catch (ex) {
 
+                } finally {
+                    this.setState({
+                        loading: false
+                    });
+                }
+            }
+        } else {//叶子节点
+            this.setState({
+                value: item.oid,
+            });
+        }
+    }
+    submitChange= ()=> {
+        this.props.onChange(this.state.value);
+        this.closeModal();
     }
     render() {
         const { inline, modalVisible, isVirtual, caption } = this.props;
@@ -170,6 +251,8 @@ export default class TreeDict extends PureComponent {
         if (this.context.getTopPadding && this.context.getTopPadding() > 0) {
             needTopPadding = true;
         }
+        const { path } = this.state;
+        const selectedItem = path.length > 0 ? path[path.length - 1] : null;
         return (
             <Modal
                 animationType={"slide"}
@@ -184,7 +267,7 @@ export default class TreeDict extends PureComponent {
                     <NavBar
                         className={needTopPadding ? 'nav-toppadding' : ''}
                         mode="light"
-                        icon={<Icon type="left" />}
+                        icon={<Icon name="chevron-left" />}
                         onLeftClick={this.closeModal}
                     >
                         {caption}
@@ -192,16 +275,23 @@ export default class TreeDict extends PureComponent {
                     <View
                         style={styles.content}
                     >
-                        <DictCrumbs path={this.state.path} root={caption} onPress={this.onSelectCrumb} />
-                        <View style={styles.content}>
+                        <DictCrumbs path={this.state.path} root={caption} onPress={this.onSelectItem} />
+                        <ScrollView style={styles.content}>
                             {
                                 (isVirtual || this.state.loading) ? <ActivityIndicator /> : null
                             }
                             {
-
+                                <DictItemList
+                                    items={selectedItem ? selectedItem.children : null}
+                                    onDictItemSelect={this.onSelectItem}
+                                    value={this.state.value}
+                                />
                             }
-                        </View>
+                        </ScrollView>
                     </View>
+                    <NavBar 
+                        rightContent={<TouchableHighlight onPress={this.submitChange}><Icon style={styles.confirm} name="check"/></TouchableHighlight>}
+                    />
                 </View>
             </Modal>
         );
